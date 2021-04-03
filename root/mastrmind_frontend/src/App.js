1f4b82b5240e41a3b94caf40398e383a964bb0c7
Reactionz <lmarquez12@csub.edu>
2021-04-03 13:30:43 -0700

    refactored authentication, added fulcalendar (need to tie events to users), working on profile page, other things that I forget that have also been fixed etc.

--- root/mastrmind_frontend/src/App.js
+++ root/mastrmind_frontend/src/App.js
@@ -9,44 +9,6 @@ axios.defaults.withCredentials = true; // MAKE SURE THIS INCLUDED OR IT WILL NOT
 
 
 function App() {
-    
-    // const [userData, setUserData ] = useState({
-    //     token: undefined,
-    //     user: undefined,
-    // });
-    // return <div>App</div>
-
-    // useEffect( () => {
-    //         const loginCheck = async () => {
-    //             let token = localStorage.getItem("auth-token");
-    //             if (token === null) { 
-    //                 localStorage.setItem("auth-token", "");
-    //                 token = "";
-    //             }
-
-    //             const tokenResponse = await axios.post(
-    //                 "http://localhost:3001/users/isTokenValid", 
-    //                 null,
-    //                 { headers: { "x-auth-token": token} }
-    //             );
-                
-    //             console.log(tokenResponse)
-    //             if (tokenResponse.data) {
-    //                 const userResponse = await axios.get(
-    //                     "http://localhost:3001/users/",
-    //                     { headers: { "x-auth-token": token }, 
-    //                 });     
-                    
-    //                 setUserData({
-    //                     token,
-    //                     user: userResponse.data,
-    //                 });
-    //             }
-    //         };  
-
-    //         loginCheck();
-    // }, []);
-
     return (
         <AuthContextProvider>
             <Router/>
