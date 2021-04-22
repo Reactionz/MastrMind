b055bf5d3107bdb9c7a78235bb1243d927aa481f
Adara <73359746+Princ3ssAdara@users.noreply.github.com>
2021-04-22 15:28:10 -0700

    Update: added ability to connect to DB and cosmetic fixes
    Changed from editor state to content state with imported convertfromraw that will allow us to connect to DB and save journal entries.
    Next step is to connect to backend.
    Cosmetic fixes include color picker (now takes up the entire top of the screen), adding more button options rather than drop down menus in the toolbar, and adding a border around the toolbar along with refactoring so that its more readable and coherent.

--- TextEditor.js
+++ TextEditor.js
@@ -2,38 +2,51 @@ import React, {Component} from 'react';
 import {render} from 'react-dom';
 import {EditorState} from "draft-js";
 import {Editor} from "react-draft-wysiwyg";
-import {SketchPicker} from 'react-color';
+import {HuePicker} from 'react-color';
+import {convertFromRaw} from 'draft-js';
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import './App.css';
+
+//RawDraftContentState allows data to be saved in a database so we can save our journal entries
+const content = {"entityMap":{},"blocks":[{"key":"637gr","text":"Initialized from content state.","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}]};
 
 class TextEditor extends Component {
     constructor(props) {
         super(props);
-        this.state = {editorState: EditorState.createEmpty(), };
+        const contentState = convertFromRaw(content);
+        this.state = {contentState};
     }
 
-    onEditorStateChange: Function = (editorState) => {
-        this.setState({editorState, });
+    onEditorStateChange: Function = (contentState) => {
+        this.setState({contentState, });
     };
 
     render() {
-        const {editorState} = this.state;
+        const {contentState} = this.state;
         return <div className ='editor'>
             <Editor
-                editorState={editorState}
-                onEditorStateChange={this.onEditorStateChange}
-                wrapperClassName="wrapper-class"
-                editorClassName="editor-class"
-                toolbarClassName="toolbar-class"
-                toolbar={{
-                    inline: {inDropdown: true},
+                editorStyle = {{
+                    padding: '10px',
+                }}
+                onContentStateChange = {this.oncontentStateChange}
+                wrapperStyle = {{
+                    width: "cover",
+                    alignItems: "center",
+                    //padding: '10px', save for editor only
+                }}
+                wrapperClassName = "wrapper-class"
+                editorClassName = "editor-class"
+                toolbarClassName = "toolbar-class"
+                toolbarStyle = {{border: "2px solid black"}}
+                toolbar = {{
+                    //inline: {inDropdown: true},      looks nicer and fills the screen more
+                    //block: {inDropdown: true},       w/o these in a dropdown menu
                     list: {inDropdown: true},
                     textAlign: {inDropdown: true},
                     link: {inDropdown: true},
                     history: {inDropdown: true},
                     colorPicker: {inDropdown: true},
                 }}
-                wrapper={{}}
             />
         </div>
     }
