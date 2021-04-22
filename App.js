8897442b77a65e5fb07699f1b59a5351d61d14d9
Adara <73359746+Princ3ssAdara@users.noreply.github.com>
2021-04-22 15:34:16 -0700

    Update: Changing components
    Moved from a class-based component to a functional-based one so that I could use a hook for the HuePicker.
    Hooks don't work on class based components (oops) and this allowed me to remove excess files after they broke.
    Removed unnecessary imports
    Changed the divs to style here since app.css was still giving me trouble

--- App.js
+++ App.js
@@ -1,18 +1,33 @@
-import React, {Component} from 'react';
-import BackgroundColor from './Background';
+import React, {Component, useState} from 'react';
+import {HuePicker} from 'react-color';
 import TextEditor from './TextEditor';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <header><h1> Journal </h1></header>
-        <TextEditor />
-      </div>
-    );
-  }
-}
+export default function App() {
+  const [color, setColor] = useState('#1d56f0');
 
-export default App;
+  return (
+    <div>
+      <HuePicker
+        color={color}
+        //height = '0px'
+        width = 'cover' //huepicker taking up the top of the screen is more fun than a hidden bar or button
+        onChangeComplete = { (color) => {setColor(color.hex)} } 
+      />
+
+      <div style = {{
+        alignItems: 'center',
+        backgroundColor: color,
+        height:'100px',
+        padding: '40px',
+        border: 'none',
+      }} >
+        <header><h1> Journal </h1></header>
+      </div>
+
+      <TextEditor />
+
+    </div>
+  );
+}
