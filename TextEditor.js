49a69c80340ae7343d367aa8efe5202d517c5803
Adara <73359746+Princ3ssAdara@users.noreply.github.com>
2021-04-22 12:56:27 -0700

    Update to Text Editor, added color picker for text
    Added color picker for text, now the users can write in different colors
    Still working on implementing the option to change the color of the background
    Trying to decide if I want to allow the users to change the background of the "header" or the "text box", or both.
    Leaning towards just the header so that users can't accidently make their text invisible by choosing the same color for the text and text box...

--- TextEditor.js
+++ TextEditor.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import {render} from 'react-dom';
 import {EditorState} from "draft-js";
 import {Editor} from "react-draft-wysiwyg";
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";  //include css line per github formatting issue
+import {SketchPicker} from 'react-color';
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import './App.css';
 
 class TextEditor extends Component {
@@ -21,15 +22,18 @@ class TextEditor extends Component {
             <Editor
                 editorState={editorState}
                 onEditorStateChange={this.onEditorStateChange}
-                wrapperClassName="wrapperClassName"
-                editorClassName="editorClassName"
+                wrapperClassName="wrapper-class"
+                editorClassName="editor-class"
+                toolbarClassName="toolbar-class"
                 toolbar={{
                     inline: {inDropdown: true},
                     list: {inDropdown: true},
                     textAlign: {inDropdown: true},
                     link: {inDropdown: true},
-                    history: {inDropdown: true}
+                    history: {inDropdown: true},
+                    colorPicker: {inDropdown: true},
                 }}
+                wrapper={{}}
             />
         </div>
     }
