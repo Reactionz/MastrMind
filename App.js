5dda6db84e1d4a12e88995c8c5cd4bbb1ee95012
Adara <73359746+Princ3ssAdara@users.noreply.github.com>
2021-04-22 09:11:54 -0700

    Update to app.js for text editor
    Updated app.js for new text editor functionality
    Working on incorporating the color changing option in the text editor toolbar

--- App.js
+++ App.js
@@ -1,29 +1,18 @@
-import React, { useState } from 'react';
-import { EditorState } from 'draft.js';
-import { ContentState, Editor } from 'react-draft-wysiwyg';
-
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import React, {Component} from 'react';
+import BackgroundColor from './Background';
+import TextEditor from './TextEditor';
+import logo from './logo.svg';
 import './App.css';
 
-const App = () => 
-{
-  let _contentState = ContentState.createFromText('Sample');
-  const raw = convertToRaw(_contentState)
-  const [ContentState, setContentState] = useState(raw)
-  return (
-    <div className="App">
-      <header className="App-header">
-        Notes
-      </header>
-      <Editor
-        wrapperClassName="wrapper-class"
-        editorClassName="editor-class"
-        toolbarClassName="toolbar-class"
-        defaultContentState={ContentState}
-        onContentStateChange={setContentState}
-      />
-    </div>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header><h1> Journal </h1></header>
+        <TextEditor />
+      </div>
+    );
+  }
 }
 
 export default App;
