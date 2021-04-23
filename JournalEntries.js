992a10111e4a34ea7b4a953621ae6c0255ecd43b
Adara <73359746+Princ3ssAdara@users.noreply.github.com>
2021-04-23 08:49:24 -0700

    Update: connection to backend now working
    Added mongoose and successfully connected locally
    Added code from index.js from mastrmind backend

--- JournalEntries.js
+++ JournalEntries.js
@@ -1,10 +1,34 @@
-const express = require('express')
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+require("dotenv").config();
+
 const app = express();
+app.use(express.json());
 
-app.listen(3000, function() {
-    console.log('listening on 3000')
-})
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+}));
 
-app.get('/', (req, res) => {
-    res.sendFile('Users/adara/Documents/Project box/plsstop/src/Models/JournalEntries.js' + '/index.html')
-})
+app.use(cookieParser());
+
+const PORT = process.env.NODE_ENV === 'production' ? (process.env.PORT || 30) : 3001;
+
+app.listen(PORT, () => {
+    console.log(`the server is listening at ${PORT}!`)
+});
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useCreateIndex: true
+    
+}, (err) => {
+    if(err) throw err;
+    console.log("Connected to Mongo");
+});
+
+app.use("/auth", require("./routes/user_router"));
