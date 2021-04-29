d3b7bd65c78a46956f227db7e93ec8db4cdef7ea
Adara <73359746+Princ3ssAdara@users.noreply.github.com>
2021-04-29 14:24:34 -0700

    Update index.js

--- root/mastrmind_frontend/src/index.js
+++ root/mastrmind_frontend/src/index.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, {Component} from 'react';
 import ReactDOM from "react-dom"
 import App from "./App"
+import mongoose from 'mongodb';
+import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
+import {EditorState} from "draft-js";
+import {Editor} from "react-draft-wysiwyg";
+import './index.css';
 
 ReactDOM.render(
     <React.StrictMode>
