57714ce8d4a55e9104a21933222e54aededecc6f
marcjimeno <jimenomarc@gmail.com>
2021-05-17 10:50:10 -0700

    trying to fix commits on github

---
+++ root/mastrmind_frontend/src/components/pages/home.js
@@ -0,0 +1,43 @@
+/*import React, { useContext }from 'react';
+import { useHistory } from 'react-router-dom';
+import { Typography } from '@material-ui/core';
+import AuthContext from "../../context/user_context";
+import Calendar from './calendar'
+
+export default function Home() {
+    
+    const history = useHistory();
+    const { loggedIn } = useContext(AuthContext);
+
+    return (
+        
+        <>
+        { loggedIn === true && (
+        <div>
+            <Typography variant = "h3" >Welcome to Mastrmind! You're Logged In! </Typography>
+            <Calendar />
+        </div>
+        
+        )}
+        { loggedIn === false && (
+        <div>
+            <Typography variant = "h3"> Welcome to Mastrmind! Please Login! </Typography>
+        </div>
+        )}
+        </>
+    );
+}*/
+
+import React from 'react'
+import Calendar from './calendar'
+import Leaderboard from '../layout/leaderboard'
+
+function Home() {
+    return (
+        <div>
+            <Leaderboard />
+        </div>
+    )
+}
+
+export default Home
