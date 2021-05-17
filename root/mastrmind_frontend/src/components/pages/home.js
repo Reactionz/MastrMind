ffc4320169a5c31c8edf5268dd9d08521a0b97e9
marcjimeno <jimenomarc@gmail.com>
2021-05-17 10:50:10 -0700

    fixing commits

--- root/mastrmind_frontend/src/components/pages/home.js
+++ root/mastrmind_frontend/src/components/pages/home.js
@@ -18,9 +18,24 @@ export default function Home() {
             <Typography variant = "h3"> Welcome to Mastrmind! Please Login! </Typography>
         </div>
         )}
+<<<<<<< HEAD
         { loggedIn === undefined && (
         <div>
             <Typography variant = "h3"> Welcome to Mastrmind! Please Login! </Typography>
+=======
+        </>
+    );
+}*/
+
+import React from 'react'
+import Calendar from './calendar'
+import Leaderboard from '../layout/leaderboard'
+
+function Home() {
+    return (
+        <div>
+            <Leaderboard />
+>>>>>>> d4d6bd6 (redid the leaderboard to be less cluttered and easier to insert)
         </div>
         )}
         </>
